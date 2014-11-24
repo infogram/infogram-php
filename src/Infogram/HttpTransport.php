b940c7bf53a819a8915b1b7aa5f8e1f1c53f2f9c
Martins Barinskis <mb@infogr.am>
2014-11-24 17:31:24 +0200

    Relies on Guzzle HTTP library instead of cURL

--- src/Infogram/HttpTransport.php
+++ src/Infogram/HttpTransport.php
@@ -2,79 +2,37 @@
 
 namespace Infogram;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Client;
+
 class HttpTransport implements Transport
 {
+    private $client;
 
-    private $curl;
-    
-    public function __construct($curl = null)
+    public function __construct($client = null)
     {
-        $this->curl = $curl ? $curl : new DefaultCurl();
+        $this->client = $client ? $client : new Client();
     }
     
     public function send(Request $request)
     {
-        if (!$this->curl->init()) {
-            throw new \ErrorException('Could not initialize cURL');
-        }
-
         $url = $request->getUrl();
         $params = $request->getParameters();
-        $first = true;
-        $queryString = '';
-        foreach ($params as $name => $value) {
-            if (!$first) {
-                $queryString .= '&';
-            }
-            $queryString .= $this->curl->escape($name) . '=' . $this->curl->escape($value);
-            $first = false;
-        }
 
         $method = $request->getMethod();
-        
-        if (!empty($queryString)) {
-            if ($method == 'GET' || $method == 'DELETE') {
-                $url .= '?' . $queryString;
-            }
-            else if ($method == 'POST' || $method == 'PUT') {
-                $this->curl->setOption(CURLOPT_POSTFIELDS, $queryString);
-                $this->curl->setOption(CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-            }
-        }
+        $paramsKey = $method === 'GET' || $method === 'DELETE' ? 'query' : 'body';
 
-        $this->curl->setOption(CURLOPT_URL, $url);
-        $this->curl->setOption(CURLOPT_CUSTOMREQUEST, $method);
-        $this->curl->setOption(CURLOPT_RETURNTRANSFER, TRUE);
-        $this->curl->setOption(CURLOPT_HEADER, TRUE);
+        $requestOptions = array();
+        $requestOptions[$paramsKey] = $params;
+        $httpRequest = $this->client->createRequest($request->getMethod(), $url, $requestOptions);
 
-        $result = $this->curl->exec();
-        
-        if ($result === FALSE) {
-            $error = $this->curl->getError();
-            $this->curl->close();
-            throw new \ErrorException('Could not execute cURL request: ' . $error);
-        }
+        $httpResponse = $this->client->send($httpRequest);
 
-        $status = $this->curl->getInfo(CURLINFO_HTTP_CODE);
-        $this->curl->close();
-
-        $headersAndBody = explode("\r\n\r\n", $result, 2);
         $headers = array();
-        $body = '';
-        if (count($headersAndBody) == 1) {
-            $body = $headersAndBody[0];
-        }
-        else {
-            $headerLines = explode("\r\n", $headersAndBody[0]);
-            foreach ($headerLines as $line) {
-                $nameValue = explode(': ', $line);
-                if (count($nameValue) == 2) {
-                    $headers[$nameValue[0]] = $nameValue[1];
-                }
-            }
-            $body = $headersAndBody[1];
+        foreach ($httpResponse->getHeaders() as $name => $value) {
+            $headers[$name] = implode(', ', $value);
         }
 
-        return new SimpleResponse($body, $headers, $status);
+        return new SimpleResponse($httpResponse->getBody(), $headers, $httpResponse->getStatusCode());
     }
 }
